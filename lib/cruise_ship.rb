92f47d400fa611d6dc8506254ebed222f9127b33
Romario Gustave <romagustave10@gmail.com>
2019-11-15 13:54:18 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,4 +9,9 @@ suite_d: "Charlie Chaplin",
 
 def select_winner(passengers)
   # add the code snippet here!
+  winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
 end
